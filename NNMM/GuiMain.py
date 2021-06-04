a9a3b00eebb11ee6dc18c23589c630ae22e6ff98
shift <fantasy_world4423-git@yahoo.co.jp>
2021-06-04 21:04:43 +0900

    メインウィンドウ設定WIP

--- NNMM/GuiMain.py
+++ NNMM/GuiMain.py
@@ -5,8 +5,32 @@ from pathlib import Path
 
 import PySimpleGUI as sg
 
-# 対象サイト
-target = ["pixiv", "nijie", "seiga"]
+# 左ペイン
+l_pane = [
+    [sg.Listbox(["a", "b", "c"], key="-LIST-", enable_events=False, size=(40, 100), auto_size_text=True)]
+]
+
+# 右ペイン
+table_cols = ["No.", "  動画ID  ", "               動画名               ", "      投稿者      ", "  状況  ", "  投稿日時  "]
+cols_width = [20, 20, 20, 20, 80, 80]
+def_data = [['x', '0', '0', '0', '00', '0']]
+table_style = {
+    'values': def_data,
+    'headings': table_cols,
+    'max_col_width': 500,
+    # 'def_col_width': 72 // len(cols_width),
+    'def_col_width': cols_width,
+    "size": (1000, 1000),
+    "auto_size_columns": True,
+    "bind_return_key": True,
+    'key': '-TABLE-'
+}
+t = sg.Table(**table_style)
+ip = sg.Input("", size=(90, 100))
+r_pane = [
+    [ip, sg.Button("終了", key="-EXIT-")],
+    [sg.Column([[t]], expand_x=True)],
+]
 
 
 def GuiMain():
@@ -18,25 +42,22 @@ def GuiMain():
     }
 
     # ウィンドウのレイアウト
+    mf = sg.Frame("F1",
+                  [
+                      [sg.Column(l_pane, expand_x=True), sg.Column(r_pane, expand_x=True, element_justification="right")]
+                  ], size=(1070, 100))
     layout = [
-        [sg.Text("NNMM")],
-        [sg.Text("対象サイト", size=(18, 1)), sg.Combo(target, key="-TARGET-", enable_events=True, default_value=target[0])],
-        [sg.Text("作品ページURL形式", size=(18, 1)), sg.Text(target_url_example[target[0]], key="-WORK_URL_SAMPLE-", size=(32, 1))],
-        [sg.Text("作品ページURL", size=(18, 1)), sg.InputText(key="-WORK_URL-", default_text="")],
-        [sg.Text("保存先パス", size=(18, 1)), sg.InputText(key="-SAVE_PATH-", default_text=Path(__file__).parent), sg.FolderBrowse("参照", initial_folder=Path(__file__).parent, pad=((3, 0), (0, 0)))],
-        [sg.Text("", size=(53, 1)), sg.Button("実行", key="-RUN-", pad=((7, 2), (0, 0))), sg.Button("終了", key="-EXIT-")],
-        [sg.Output(key="-OUTPUT-", size=(100, 10))],
+        [mf],
     ]
 
     # ウィンドウオブジェクトの作成
-    window = sg.Window("NNMM", layout, size=(640, 320), finalize=True)
-    # window["-WORK_URL-"].bind("<FocusIn>", "+INPUT FOCUS+")
+    window = sg.Window("NNMM", layout, size=(1070, 900), finalize=True, resizable=True)
+    # window["-LIST-"].bind("<Double-Button1>", "-LIST_D-")
+    window['-LIST-'].bind('<Double-Button-1>', "+DOUBLE CLICK+")
 
     logging.config.fileConfig("./log/logging.ini", disable_existing_loggers=False)
     logger = getLogger("root")
     logger.setLevel(INFO)
-
-    print("---ここにログが表示されます---")
 
     # イベントのループ
     while True:
@@ -46,6 +67,14 @@ def GuiMain():
         # ウィンドウの×ボタンが押されれば終了
         if event in [sg.WIN_CLOSED, "-EXIT-"]:
             break
+        if event == "-LIST-+DOUBLE CLICK+":
+            v = values["-LIST-"][0]
+            def_data = window['-TABLE-'].Values
+            a_data = [v, '0', '0', '0', '00', '0']
+            def_data.append(a_data)
+            window['-TABLE-'].update(values=def_data)
+            print("ok")
+            pass
         if event == "-TARGET-":
             work_kind = values["-TARGET-"]
             # print(target_url_example[work_kind])
