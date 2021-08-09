62fa1bff3fac047d6736d48b898b38c634ed0353
shift <fantasy_world4423-git@yahoo.co.jp>
2021-08-09 14:26:15 +0900

    タイマーセットに失敗することがあったため修正

--- NNMM/Timer.py
+++ NNMM/Timer.py
@@ -16,18 +16,18 @@ from NNMM.ConfigMain import ProcessConfigBase
 logger = getLogger("root")
 logger.setLevel(INFO)
 
-timer_thread = None
-
 
 class ProcessTimer(ProcessBase.ProcessBase):
+    timer_thread = None
+    window = None
+    values = None
 
     def __init__(self):
         super().__init__(False, False, "タイマーセット")
 
     def Run(self, mw):
-        global timer_thread
-        window = mw.window
-        values = mw.values
+        self.window = mw.window
+        self.values = mw.values
 
         # タイマーセットイベントが登録された場合
         # v = values["-TIMER_SET-"]
@@ -38,9 +38,9 @@ class ProcessTimer(ProcessBase.ProcessBase):
         config = ProcessConfigBase.GetConfig()
         i_str = config["general"].get("auto_reload", "")
         if i_str == "(使用しない)" or i_str == "":
-            if timer_thread:
-                timer_thread.cancel()
-                timer_thread = None
+            if self.timer_thread:
+                self.timer_thread.cancel()
+                self.timer_thread = None
             return
 
         pattern = "^([0-9]+)分毎$"
@@ -48,30 +48,30 @@ class ProcessTimer(ProcessBase.ProcessBase):
 
         # 既に更新中なら二重に実行はしない
         pattern = "^更新中\([0-9]+\/[0-9]+\)$|^更新中$"
-        v = window["-INPUT2-"].get()
-        if values["-TIMER_SET-"] == "-FIRST_SET-":
-            values["-TIMER_SET-"] = ""
+        v = self.window["-INPUT2-"].get()
+        if self.values.get("-TIMER_SET-") == "-FIRST_SET-":
+            self.values["-TIMER_SET-"] = ""
             logger.info("Auto-reload -FIRST_SET- ... skip first auto-reload cycle.")
 
-            if timer_thread:
-                timer_thread.cancel()
-                timer_thread = None
+            if self.timer_thread:
+                self.timer_thread.cancel()
+                self.timer_thread = None
 
         elif re.search(pattern, v):
-            values["-TIMER_SET-"] = ""
+            self.values["-TIMER_SET-"] = ""
             logger.info("-ALL_UPDATE- running now ... skip this auto-reload cycle.")
             pass
         else:
             # すべて更新ボタンが押された場合の処理を起動する
-            window.write_event_value("-ALL_UPDATE-", "")
+            self.window.write_event_value("-ALL_UPDATE-", "")
 
         # タイマーをセットして起動
         s_interval = interval * 60  # [min] -> [sec]
         # s_interval = 5  # デバッグ用
-        timer_thread = threading.Timer(s_interval, self.Run, (mw, ))
+        self.timer_thread = threading.Timer(s_interval, self.Run, (mw, ))
         # デーモンスレッドはデーモンスレッド以外のスレッドが動いていない場合に自動的に終了される
-        timer_thread.setDaemon(True)
-        timer_thread.start()
+        self.timer_thread.setDaemon(True)
+        self.timer_thread.start()
 
         dts_format = "%Y-%m-%d %H:%M:%S"
         dst = datetime.now() + timedelta(minutes=interval)
