879b8e827f104c28079adda034fd9a4bba4571b2
shift <fantasy_world4423-git@yahoo.co.jp>
2021-08-13 07:15:27 +0900

    DB操作マルチスレッド対応wip

--- NNMM/Process/ProcessUpdateAllMylistInfo.py
+++ NNMM/Process/ProcessUpdateAllMylistInfo.py
@@ -8,6 +8,7 @@ import PySimpleGUI as sg
 
 from NNMM.MylistDBController import *
 from NNMM.MylistInfoDBController import *
+from NNMM.MultiDBController import *
 from NNMM.GuiFunction import *
 from NNMM.Process.ProcessUpdateMylistInfo import *
 from NNMM.Process import ProcessBase
@@ -85,90 +86,103 @@ class ProcessUpdateAllMylistInfo(ProcessUpdateMylistInfo):
 
         # すべてのマイリストの情報を更新する
         self.done_count = 0
+        self.old_mylist_db = self.mylist_db
+        self.old_mylist_info_db = self.mylist_info_db
+        self.mylist_db = MylistDBCM(self.mylist_db.dbname)
+        self.mylist_info_db = MylistInfoDBCM(self.mylist_info_db.dbname)
+
         for m, prev_video_list in zip(m_list, prev_video_lists):
-            mylist_url = m.get("url")
-            records = [r[1] for r in result if r[0] == mylist_url]
-            if len(records) == 1:
-                records = records[0]
-            else:
-                # error
-                continue
+            self.Working(m, prev_video_list, result)
 
-            if len(records) == 0:
-                # error
-                # 新規マイリスト取得でレンダリングが失敗した場合など
-                continue
-
-            # prev_video_list = self.mylist_info_db.SelectFromMylistURL(mylist_url)
-            prev_videoid_list = [m["video_id"] for m in prev_video_list]
-            prev_username = ""
-            if prev_video_list:
-                prev_username = prev_video_list[0].get("username")
-            now_video_list = records
-            now_videoid_list = [m["video_id"] for m in now_video_list]
-
-            # 状況ステータスを調べる
-            status_check_list = []
-            add_new_video_flag = False
-            for i, n in enumerate(now_videoid_list):
-                if n in prev_videoid_list:
-                    # 以前から保持していた動画が取得された場合
-                    s = [p["status"] for p in prev_video_list if p["video_id"] == n]
-                    status_check_list.append(s[0])
-                else:
-                    # 新規に動画が追加された場合
-                    status_check_list.append("未視聴")
-                    add_new_video_flag = True
-
-            # 右ペインのテーブルに表示するマイリスト情報を取得
-            def_data = []
-            table_cols = ["no", "id", "title", "username", "status", "uploaded", "video_url"]
-
-            # 右ペインのテーブルにマイリスト情報を表示
-            for m, s in zip(now_video_list, status_check_list):
-                m["status"] = s
-                a = [m["no"], m["video_id"], m["title"], m["username"], m["status"], m["uploaded"]]
-                def_data.append(a)
-            if self.window["-INPUT1-"].get() == mylist_url:
-                now_show_table_data = list[def_data]
-
-            # usernameが変更されていた場合
-            if now_video_list:
-                now_username = now_video_list[0].get("username")
-                if prev_username != now_username:
-                    # マイリストの名前を更新する
-                    self.mylist_db.UpdateUsername(mylist_url, now_username)
-                    # 格納済の動画情報の投稿者名を更新する
-                    self.mylist_info_db.UpdateUsernameInMylist(mylist_url, now_username)
-
-            # DBに格納
-            records = []
-            for m in now_video_list:
-                dst = GetNowDatetime()
-                r = {
-                    "video_id": m["video_id"],
-                    "title": m["title"],
-                    "username": m["username"],
-                    "status": m["status"],
-                    "uploaded_at": m["uploaded"],
-                    "video_url": m["video_url"],
-                    "mylist_url": m["mylist_url"],
-                    "created_at": dst
-                }
-                records.append(r)
-            self.mylist_info_db.UpsertFromList(records)
-
-            # マイリストの更新日時更新
-            if add_new_video_flag:
-                dst = GetNowDatetime()
-                self.mylist_db.UpdateUpdatedAt(mylist_url, dst)
-
-            self.done_count = self.done_count + 1
-            p_str = f"更新中({self.done_count}/{all_index_num})"
-            self.window.write_event_value("-ALL_UPDATE_THREAD_PROGRESS-", p_str)
-            logger.info(mylist_url + f" : update done ... ({self.done_count}/{all_index_num}).")
+        self.mylist_db = self.old_mylist_db
+        self.mylist_info_db = self.old_mylist_info_db
 
         self.window.write_event_value("-ALL_UPDATE_THREAD_DONE-", "")
+
+    def Working(self, m, prev_video_list, now_video_lists):
+        mylist_url = m.get("url")
+        records = [r[1] for r in now_video_lists if r[0] == mylist_url]
+        if len(records) == 1:
+            records = records[0]
+        else:
+            # error
+            return
+
+        if len(records) == 0:
+            # error
+            # 新規マイリスト取得でレンダリングが失敗した場合など
+            return
+
+        # prev_video_list = self.mylist_info_db.SelectFromMylistURL(mylist_url)
+        prev_videoid_list = [m["video_id"] for m in prev_video_list]
+        prev_username = ""
+        if prev_video_list:
+            prev_username = prev_video_list[0].get("username")
+        now_video_list = records
+        now_videoid_list = [m["video_id"] for m in now_video_list]
+
+        # 状況ステータスを調べる
+        status_check_list = []
+        add_new_video_flag = False
+        for i, n in enumerate(now_videoid_list):
+            if n in prev_videoid_list:
+                # 以前から保持していた動画が取得された場合
+                s = [p["status"] for p in prev_video_list if p["video_id"] == n]
+                status_check_list.append(s[0])
+            else:
+                # 新規に動画が追加された場合
+                status_check_list.append("未視聴")
+                add_new_video_flag = True
+
+        # 右ペインのテーブルに表示するマイリスト情報を取得
+        def_data = []
+        table_cols = ["no", "id", "title", "username", "status", "uploaded", "video_url"]
+
+        # 右ペインのテーブルにマイリスト情報を表示
+        for m, s in zip(now_video_list, status_check_list):
+            m["status"] = s
+            a = [m["no"], m["video_id"], m["title"], m["username"], m["status"], m["uploaded"]]
+            def_data.append(a)
+        if self.window["-INPUT1-"].get() == mylist_url:
+            now_show_table_data = list[def_data]
+
+        # usernameが変更されていた場合
+        if now_video_list:
+            now_username = now_video_list[0].get("username")
+            if prev_username != now_username:
+                # マイリストの名前を更新する
+                self.mylist_db.UpdateUsername(mylist_url, now_username)
+                # 格納済の動画情報の投稿者名を更新する
+                self.mylist_info_db.UpdateUsernameInMylist(mylist_url, now_username)
+
+        # DBに格納
+        records = []
+        for m in now_video_list:
+            dst = GetNowDatetime()
+            r = {
+                "video_id": m["video_id"],
+                "title": m["title"],
+                "username": m["username"],
+                "status": m["status"],
+                "uploaded_at": m["uploaded"],
+                "video_url": m["video_url"],
+                "mylist_url": m["mylist_url"],
+                "created_at": dst
+            }
+            records.append(r)
+        self.mylist_info_db.UpsertFromList(records)
+
+        # マイリストの更新日時更新
+        if add_new_video_flag:
+            dst = GetNowDatetime()
+            self.mylist_db.UpdateUpdatedAt(mylist_url, dst)
+
+        self.done_count = self.done_count + 1
+        all_index_num = len(now_video_lists)
+        p_str = f"更新中({self.done_count}/{all_index_num})"
+        self.window.write_event_value("-ALL_UPDATE_THREAD_PROGRESS-", p_str)
+        logger.info(mylist_url + f" : update done ... ({self.done_count}/{all_index_num}).")
+        return 0
 
 
 class ProcessUpdateAllMylistInfoThreadProgress(ProcessBase.ProcessBase):
