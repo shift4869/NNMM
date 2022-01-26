c74e54c12dc81cc0ce586b5cc8887cd3d11a74c8
shift <fantasy_world4423-git@yahoo.co.jp>
2022-01-26 16:39:19 +0900

    マイリスト移動（下）のテスト追加

--- NNMM/Process/ProcessMoveDown.py
+++ NNMM/Process/ProcessMoveDown.py
@@ -19,23 +19,40 @@ class ProcessMoveDown(ProcessBase.ProcessBase):
         super().__init__(True, True, "下に移動")
 
     def Run(self, mw):
-        # "下に移動::-MR-"
-        # マイリスト右クリックで「下に移動」が選択された場合
-        self.window = mw.window
-        self.values = mw.values
-        self.mylist_db = mw.mylist_db
-        self.mylist_info_db = mw.mylist_info_db
+        """マイリストの並びを下に移動させる
+
+        Notes:
+            "下に移動::-MR-"
+            マイリスト右クリックで「下に移動」が選択された場合
+
+        Args:
+            mw (MainWindow): メインウィンドウオブジェクト
+
+        Returns:
+            int: 移動に成功した場合0,
+                 一番下のマイリストが選択され下に移動できなかった場合1,
+                 エラー時-1
+        """
+        # 引数チェック
+        try:
+            self.window = mw.window
+            self.values = mw.values
+            self.mylist_db = mw.mylist_db
+            self.mylist_info_db = mw.mylist_info_db
+        except AttributeError:
+            logger.error("MoveDown failed, argument error.")
+            return -1
 
         src_index = 0
         if self.window["-LIST-"].get_indexes():
             src_index = self.window["-LIST-"].get_indexes()[0]
         src_v = self.values["-LIST-"][0]  # ダブルクリックされたlistboxの選択値
-        list_data = self.window["-LIST-"].Values  # 現在のtableの全リスト
+        list_data = self.window["-LIST-"].Values  # 現在のマイリストテーブルの全リスト
 
         max_index = len(self.mylist_db.Select()) - 1
         if src_index >= max_index:
             logger.info(f"{src_v} -> index is {max_index} , can't move down.")
-            return
+            return 1
 
         if src_v[:2] == "*:":
             src_v = src_v[2:]
@@ -54,6 +71,7 @@ class ProcessMoveDown(ProcessBase.ProcessBase):
         self.window["-LIST-"].update(set_to_index=dst_index)
 
         logger.info(f"{src_v} -> index move down from {src_index} to {dst_index}.")
+        return 0
 
 
 if __name__ == "__main__":
