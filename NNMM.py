fab1f0f6add655096fba43ef507ce131c58fa44f
shift <fantasy_world4423-git@yahoo.co.jp>
2024-06-04 07:20:43 +0900

    rye適用1

--- NNMM.py
+++
@@ -1,7 +0,0 @@
-import NNMM.main_window
-
-# python NNMM.py
-
-if __name__ == "__main__":
-    mw = NNMM.main_window.MainWindow()
-    mw.run()
---
+++ src/nnmm/main.py
@@ -0,0 +1,7 @@
+import nnmm.main_window
+
+# python NNMM.py
+
+if __name__ == "__main__":
+    mw = nnmm.main_window.MainWindow()
+    mw.run()
