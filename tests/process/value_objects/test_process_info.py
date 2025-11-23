7bb96f15eadf60ff37332f6bc50eeb8f0ec7d119
shift <fantasy_world4423-git@yahoo.co.jp>
2025-11-23 17:42:18 +0900

    process_infoテスト修正

--- tests/process/value_objects/test_process_info.py
+++ tests/process/value_objects/test_process_info.py
@@ -11,63 +11,50 @@ from nnmm.process.value_objects.process_info import ProcessInfo
 
 
 class TestProcessInfo(unittest.TestCase):
-    @unittest.skip("")
     def test_init(self):
         process_name = "-TEST_PROCESS-"
         window = MagicMock(spec=QDialog)
-        values = MagicMock(spec=dict)
         mylist_db = MagicMock(spec=MylistDBController)
         mylist_info_db = MagicMock(spec=MylistInfoDBController)
-        actual = ProcessInfo(process_name, window, values, mylist_db, mylist_info_db)
+        actual = ProcessInfo(process_name, window, mylist_db, mylist_info_db)
         self.assertEqual(process_name, actual.name)
         self.assertEqual(window, actual.window)
-        self.assertEqual(values, actual.values)
         self.assertEqual(mylist_db, actual.mylist_db)
         self.assertEqual(mylist_info_db, actual.mylist_info_db)
 
         params_list = [
-            (-1, window, values, mylist_db, mylist_info_db),
-            (process_name, -1, values, mylist_db, mylist_info_db),
+            (-1, window, mylist_db, mylist_info_db),
+            (process_name, -1, mylist_db, mylist_info_db),
             (process_name, window, -1, mylist_db, mylist_info_db),
-            (process_name, window, values, -1, mylist_info_db),
-            (process_name, window, values, mylist_db, -1),
+            (process_name, window, -1, mylist_info_db),
+            (process_name, window, mylist_db, -1),
         ]
         for params in params_list:
             with self.assertRaises(ValueError):
-                actual = ProcessInfo(params[0], params[1], params[2], params[3], params[4])
+                actual = ProcessInfo(params[0], params[1], params[2], params[3])
 
-    @unittest.skip("")
     def test_repr(self):
         process_name = "-TEST_PROCESS-"
         window = MagicMock(spec=QDialog)
-        values = MagicMock(spec=dict)
         mylist_db = MagicMock(spec=MylistDBController)
         mylist_info_db = MagicMock(spec=MylistInfoDBController)
-        process_info = ProcessInfo(process_name, window, values, mylist_db, mylist_info_db)
+        process_info = ProcessInfo(process_name, window, mylist_db, mylist_info_db)
         actual = repr(process_info)
 
         name_str = f"name={process_name}"
         window_str = f"window={id(window)}"
-        values_str = f"values={id(values)}"
         mylist_db_str = f"mylist_db={id(mylist_db)}"
         mylist_info_db_str = f"mylist_info_db={id(mylist_info_db)}"
-        expect = f"ProcessInfo({name_str}, {window_str}, {values_str}, {mylist_db_str}, {mylist_info_db_str})"
+        expect = f"ProcessInfo({name_str}, {window_str}, {mylist_db_str}, {mylist_info_db_str})"
         self.assertEqual(expect, actual)
 
-    @unittest.skip("")
     def test_create(self):
         process_name = "-TEST_PROCESS-"
-        window = MagicMock(spec=QDialog)
-        values = MagicMock(spec=dict)
         mylist_db = MagicMock(spec=MylistDBController)
         mylist_info_db = MagicMock(spec=MylistInfoDBController)
 
-        def return_main_window(window_flag=True, values_flag=True, mylist_db_flag=True, mylist_info_db_flag=True):
+        def return_main_window(mylist_db_flag=True, mylist_info_db_flag=True):
             mock_main_window = MagicMock(spec=MainWindow)
-            if window_flag:
-                mock_main_window.window = window
-            if values_flag:
-                mock_main_window.values = values
             if mylist_db_flag:
                 mock_main_window.mylist_db = mylist_db
             if mylist_info_db_flag:
@@ -76,17 +63,14 @@ class TestProcessInfo(unittest.TestCase):
 
         main_window = return_main_window()
         actual = ProcessInfo.create(process_name, main_window)
-        expect = ProcessInfo(process_name, window, values, mylist_db, mylist_info_db)
-        self.assertEqual(expect, actual)
+        self.assertIsInstance(actual, ProcessInfo)
 
         params_list = [
-            (False, True, True, True),
-            (True, False, True, True),
-            (True, True, False, True),
-            (True, True, True, False),
+            (True, False),
+            (False, True),
         ]
         for params in params_list:
-            main_window = return_main_window(params[0], params[1], params[2], params[3])
+            main_window = return_main_window(params[0], params[1])
             with self.assertRaises(ValueError):
                 actual = ProcessInfo.create(process_name, main_window)
 
